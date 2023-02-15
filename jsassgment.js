77c007779e8c5ec14d604ce9e786e7665df6ca1b
Abuhafs123 <niyi00098@gmail.com>
2023-02-15 08:53:54 +0100

    myJSpush

---
+++ jsassgment.js
@@ -0,0 +1,20 @@
+let firstNum = 10
+let secondNum = 20
+
+let sum = firstNum + secondNum
+let subtract = secondNum - firstNum
+let multiply = firstNum * secondNum
+let divide = firstNum / secondNum
+let modulus = firstNum % secondNum
+
+
+
+console.log(sum)
+console.log(subtract)
+console.log(multiply)
+console.log(divide)
+console.log(modulus)
+
+
+
+
